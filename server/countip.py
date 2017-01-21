edf30dc20eba70760b2bd9133c6d6b9436f473ee
joliu <joliu@s-an.org>
2017-01-21 20:12:43 +0800

    引用conn模块，适配python3

--- server/countip.py
+++ server/countip.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python
 #coding:utf-8
-import MySQLdb
+import conn
 def count():
-	try:
-		conn=MySQLdb.connect(host='123.206.77.218',user='root',passwd='123456',port=12306)
-		cur=conn.cursor()
-		conn.select_db('network')
-		cur.execute('select count(*) from ips where status=1')
-		result=cur.fetchone()
-		cur.close()
-		conn.close()
-		return result[0]
-	except MySQLdb.Error,e:
-		print "Mysql Error%d:%s"% (e.args[0],e.args[1])
-		return 0
+    #:统计在线的ip数
+    sql = 'select count(*) from ips where status=1'
+    result = conn.mysql_execute(sql,'fetchone')
+    return result[0]
+
+if __name__ == '__main__':
+    print(count())
