ea4dcd58ce4419d5ce08717c43ec4241b85f4dad
joliu <joliu@s-an.org>
2017-01-22 22:18:15 +0800

    修改bug

--- client/mylog.py
+++ client/mylog.py
@@ -2,8 +2,9 @@ import time
 
 #:输出log信息
 def log(msg,taskid):
+    msg = str(msg)
     print(time.asctime() + ':taskid:' + taskid +':'+msg)
 #:输出错误log信息
 def error_log(error,error_type):
-    print(time.asctime() + ':' + error_type + ':' +error)
+    print("%s:%s:%s" % (time.asctime(),error_type,error))
  
