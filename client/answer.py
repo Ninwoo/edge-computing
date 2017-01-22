1b1d8c1fa5abeafb55bedea1d2f845f395030665
joliu <joliu@s-an.org>
2017-01-22 19:56:16 +0800

    适配python3，添加注释

--- client/answer.py
+++ client/answer.py
@@ -2,19 +2,21 @@
 #coding=utf-8
 import socket
 import sys
-import commands
+import subprocess
 import re
 import time
 
 def send(host,port,msg):
-	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
-	#judge the network status by ping
-	cmd='ping -w 1 -c 1 '+host
-	(status,output)=commands.getstatusoutput(cmd)
-	regex=re.compile("time=\d*",re.IGNORECASE | re.MULTILINE)
-	if len(regex.findall(output))>0:
-		s.connect((host,port))
-	        s.send(msg)
-        	data=s.recv(1024)
-		s.close()
-		return data
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+    #:使用ping检测网络能否到达
+    cmd='ping -w 1 -c 1 '+host
+    (status,output)=subprocess.getstatusoutput(cmd)
+    regex=re.compile("time=\d*",re.IGNORECASE | re.MULTILINE)
+    if len(regex.findall(output))>0:
+        #:发送消息
+        s.connect((host,port))
+        s.send(msg)
+        data=s.recv(1024)
+        s.close()
+        #:返回得到的消息
+        return data
