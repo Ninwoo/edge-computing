e4496605dacd31a17e0d9cf35ffe4d4f71717016
joliu <joliu@s-an.org>
2017-01-26 20:32:17 +0800

    适配代码规范

--- client/cpuserver.py
+++ client/cpuserver.py
@@ -2,23 +2,23 @@
 #coding=utf-8
 import socket
 import sys,os
-import commands
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+import subprocess
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
 
-if len(sys.argv)==1:
-	print "need argv"
+if len(sys.argv) == 1:
+    print("need argv")
 else:
-	host=''
-	port=int(sys.argv[1])
-	s.bind((host,port))
-	s.listen(3)
-	while True:
-		client,ipaddr=s.accept()
-		print "Got a connect from %s"  %str(ipaddr)
-		data=client.recv(1024)
-		if data=='Cpu':
- 			sy=os.popen('top -bi -n 2 -d 0.02').read().split('\n\n')[2].split('\n')[2].split(':')[1].split(',')[1]
-			client.send(sy)
-		else:
-			client.send("echo:Failed!")
-		client.close()
+    host = ''
+    port = int(sys.argv[1])
+    s.bind((host,port))
+    s.listen(3)
+    while True:
+        client,ipaddr = s.accept()
+        print("Got a connect from %s"  %str(ipaddr))
+        data = client.recv(1024)
+        if data == 'Cpu':
+            sy = os.popen('top -bi -n 2 -d 0.02').read().split('\n\n')[2].split('\n')[2].split(':')[1].split(',')[1]
+            client.send(sy)
+        else:
+            client.send("echo:Failed!")
+            client.close()
